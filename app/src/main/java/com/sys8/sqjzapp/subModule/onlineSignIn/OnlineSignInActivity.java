1b58f84cbcea8e377d32f1554eab81a5faee3fd9
guoxw <xuewei_g@163.com>
2020-03-28 17:30:19 +0800

    地图模块加个小蓝箭头

--- app/src/main/java/com/sys8/sqjzapp/subModule/onlineSignIn/OnlineSignInActivity.java
+++ app/src/main/java/com/sys8/sqjzapp/subModule/onlineSignIn/OnlineSignInActivity.java
@@ -2,6 +2,7 @@ package com.sys8.sqjzapp.subModule.onlineSignIn;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 import com.amap.api.location.AMapLocation;
 import com.amap.api.location.AMapLocationClient;
@@ -31,6 +32,8 @@ public class OnlineSignInActivity extends BaseActivity implements AMapLocationLi
     TitleBar tbOnlinesigninTitlebar;
     @BindView(R.id.map)
     MapView mapView;
+    @BindView(R.id.tv_onlinesignin_currentlocation)
+    TextView tvOnlinesigninCurrentlocation;
 
     //地图
     private AMap aMap;
@@ -67,7 +70,6 @@ public class OnlineSignInActivity extends BaseActivity implements AMapLocationLi
         myLocationStyle.radiusFillColor(android.R.color.transparent);
         myLocationStyle.strokeColor(android.R.color.transparent);
         aMap.setMyLocationStyle(myLocationStyle);
-
     }
 
     private void initMap() {
@@ -89,6 +91,12 @@ public class OnlineSignInActivity extends BaseActivity implements AMapLocationLi
             if (amapLocation != null
                     && amapLocation.getErrorCode() == 0) {
                 mListener.onLocationChanged(amapLocation);// 显示系统小蓝点
+                String address = "";
+                address += amapLocation.getCity();//市
+                address += amapLocation.getDistrict();//区域
+                address += amapLocation.getAoiName();
+                Log.e("OnlineSignInActivity", "address:" + address);
+                tvOnlinesigninCurrentlocation.setText(address);
             } else {
                 String errText = "定位失败," + amapLocation.getErrorCode() + ": " + amapLocation.getErrorInfo();
                 Log.e("AmapErr", errText);
@@ -110,9 +118,11 @@ public class OnlineSignInActivity extends BaseActivity implements AMapLocationLi
             mLocationClient.setLocationListener(this);
             //设置为高精度定位模式
             mLocationOption.setLocationMode(AMapLocationClientOption.AMapLocationMode.Hight_Accuracy);
+            // 此方法为每隔固定时间会发起一次定位请求，为了减少电量消耗或网络流量消耗，
+            mLocationOption.setInterval(10000);
             //设置定位参数
             mLocationClient.setLocationOption(mLocationOption);
-            // 此方法为每隔固定时间会发起一次定位请求，为了减少电量消耗或网络流量消耗，
+
             // 注意设置合适的定位时间的间隔（最小间隔支持为2000ms），并且在合适时间调用stopLocation()方法来取消定位请求
             // 在定位结束后，在合适的生命周期调用onDestroy()方法
             // 在单次定位情况下，定位无论成功与否，都无需调用stopLocation()方法移除请求，定位sdk内部会移除
