a2547bd3db78f7d2119da1d33ed413b379a43d0d
1120121903 <460980971@qq.com>
2020-03-28 15:05:48 +0800

    顶部标题栏

--- app/src/main/java/com/sys8/sqjzapp/baseClass/ActivityCollector.java
+++ app/src/main/java/com/sys8/sqjzapp/baseClass/ActivityCollector.java
@@ -1,6 +1,11 @@
 package com.sys8.sqjzapp.baseClass;
 
 import android.app.Activity;
+import android.view.View;
+
+import com.hjq.bar.OnTitleBarListener;
+import com.hjq.bar.TitleBar;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,4 +25,26 @@ public class ActivityCollector {
     public  static void removeActivity(Activity activity){
         activities.remove(activity);
     }
+
+    public static void finishAll(){
+        for(Activity activity: activities){
+            activity.finish();
+        }
+    }
+
+    /**
+     *description:Title返回按钮监听函数
+    */
+    public static void TitleCilckListener(TitleBar titleBar,Activity activity){
+        titleBar.setOnTitleBarListener(new OnTitleBarListener() {
+            @Override
+            public void onLeftClick(View v) {
+                activity.finish();
+            }
+            @Override
+            public void onTitleClick(View v) { }
+            @Override
+            public void onRightClick(View v) { }});
+    }
+
 }
--- app/src/main/java/com/sys8/sqjzapp/baseClass/BaseActivity.java
+++ app/src/main/java/com/sys8/sqjzapp/baseClass/BaseActivity.java
@@ -1,10 +1,7 @@
 package com.sys8.sqjzapp.baseClass;
 
-import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
-import android.view.WindowManager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -15,10 +12,10 @@ public class BaseActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstancestate){
         super.onCreate(savedInstancestate);
-//        hideBottomUIMenu();
-        //将当前正在创建的活动添加到活动管理器中
+        /* 将当前正在创建的活动添加到活动管理器中 */
         ActivityCollector.addActivity(this);
         Log.d("BaseActivity",getClass().getSimpleName());
+
     }
     /**
      *description:重写活动销毁方法
@@ -31,21 +28,21 @@ public class BaseActivity extends AppCompatActivity {
     /**
      *description:隐藏底部虚拟手势栏，设置为全屏
      */
-    protected void hideBottomUIMenu(){
-        if (Build.VERSION.SDK_INT > 11 && Build.VERSION.SDK_INT < 19) { // lower api
-            View v = this.getWindow().getDecorView();
-            v.setSystemUiVisibility(View.GONE);
-        } else if (Build.VERSION.SDK_INT >= 19) {
-            //for new api versions.
-            View decorView = getWindow().getDecorView();
-            int uiOptions = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
-                    | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
-                    | View.SYSTEM_UI_FLAG_IMMERSIVE;
-            decorView.setSystemUiVisibility(uiOptions);
-            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
-        }
-    }
+//    protected void hideBottomUIMenu(){
+//        if (Build.VERSION.SDK_INT > 11 && Build.VERSION.SDK_INT < 19) { // lower api
+//            View v = this.getWindow().getDecorView();
+//            v.setSystemUiVisibility(View.GONE);
+//        } else if (Build.VERSION.SDK_INT >= 19) {
+//            //for new api versions.
+//            View decorView = getWindow().getDecorView();
+//            int uiOptions = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+//                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+//                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+//                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+//                    | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
+//                    | View.SYSTEM_UI_FLAG_IMMERSIVE;
+//            decorView.setSystemUiVisibility(uiOptions);
+//            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+//        }
+//    }
 }
