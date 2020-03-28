4eb17b18013313597381e14c32d3f63ebc5713b1
guoxw <xuewei_g@163.com>
2020-03-28 13:18:04 +0800

    网上签到模块初始化

--- app/src/main/java/com/sys8/sqjzapp/subModule/wsqd/WsqdActivity.java
+++ app/src/main/java/com/sys8/sqjzapp/subModule/wsqd/WsqdActivity.java
@@ -1,12 +1,10 @@
 package com.sys8.sqjzapp.subModule.wsqd;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-
 import com.sys8.sqjzapp.R;
+import com.sys8.sqjzapp.baseClass.BaseActivity;
 
-public class WsqdActivity extends AppCompatActivity {
+public class WsqdActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
