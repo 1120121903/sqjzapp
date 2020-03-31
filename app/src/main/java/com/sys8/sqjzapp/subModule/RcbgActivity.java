70bdb1aee6a7555891d04cbeee9192ac0b4f269a
1120121903 <460980971@qq.com>
2020-03-31 16:25:27 +0800

    新增人脸识别Activity和日常报告Activity

--- app/src/main/java/com/sys8/sqjzapp/subModule/RcbgActivity.java
+++ app/src/main/java/com/sys8/sqjzapp/subModule/RcbgActivity.java
@@ -1,16 +1,26 @@
 package com.sys8.sqjzapp.subModule;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import com.hjq.bar.TitleBar;
 import com.sys8.sqjzapp.R;
+import com.sys8.sqjzapp.baseClass.BaseActivity;
 
-public class RcbgActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+import static com.sys8.sqjzapp.baseClass.ActivityCollector.TitleCilckListener;
+
+public class RcbgActivity extends BaseActivity {
+
+    @BindView(R.id.tb_rcbg)
+    TitleBar tbRcbg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rcbg);
+        ButterKnife.bind(this);
+        TitleCilckListener(tbRcbg, this);/*title按钮监听*/
     }
 }
