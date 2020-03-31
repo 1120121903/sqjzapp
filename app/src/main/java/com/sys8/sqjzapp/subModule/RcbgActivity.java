d0076007cd566446b14c564086e3742028e1b1da
1120121903 <460980971@qq.com>
2020-03-31 18:44:14 +0800

    日常报告-填写报告

--- app/src/main/java/com/sys8/sqjzapp/subModule/RcbgActivity.java
+++ app/src/main/java/com/sys8/sqjzapp/subModule/RcbgActivity.java
@@ -1,8 +1,12 @@
 package com.sys8.sqjzapp.subModule;
 
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.FrameLayout;
+import android.widget.ImageButton;
 
 import com.hjq.bar.TitleBar;
 import com.sys8.sqjzapp.R;
@@ -19,10 +23,8 @@ public class RcbgActivity extends BaseActivity {
     TitleBar tbRcbg;
     @BindView(R.id.fl_rcbg)
     FrameLayout flRcbg;
-    @BindView(R.id.bt_tztx_tz)
-    Button btTztxTz;
-    @BindView(R.id.bt_tztx_tx)
-    Button btTztxTx;
+    @BindView(R.id.et_rcbg_content)
+    EditText etRcbgContent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +32,7 @@ public class RcbgActivity extends BaseActivity {
         setContentView(R.layout.activity_rcbg);
         ButterKnife.bind(this);
         TitleCilckListener(tbRcbg, this);/*title按钮监听*/
+
     }
 
 
