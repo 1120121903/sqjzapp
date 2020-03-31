4f407ad708da02887f83c3bdce0ffc1a7f0e1d95
1120121903 <460980971@qq.com>
2020-03-31 13:38:53 +0800

    日常报告

---
+++ app/src/main/java/com/sys8/sqjzapp/subModule/RcbgActivity.java
@@ -0,0 +1,16 @@
+package com.sys8.sqjzapp.subModule;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.sys8.sqjzapp.R;
+
+public class RcbgActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rcbg);
+    }
+}
