654da9a590d79293d7be73ab97bc569a6969097d
1120121903 <460980971@qq.com>
2020-03-31 13:53:02 +0800

    layout目录更新

---
+++ app/src/main/java/com/sys8/sqjzapp/common/FaceVerifyActivity.java
@@ -0,0 +1,16 @@
+package com.sys8.sqjzapp.common;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.sys8.sqjzapp.R;
+
+public class FaceVerifyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_face_verify);
+    }
+}
