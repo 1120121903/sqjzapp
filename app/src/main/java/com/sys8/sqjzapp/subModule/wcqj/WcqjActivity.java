317bccae663b67e3cdaa62be54f0ba957fa615b3
1120121903 <460980971@qq.com>
2020-04-01 19:27:49 +0800

    新增:外出请假wcqj

---
+++ app/src/main/java/com/sys8/sqjzapp/subModule/wcqj/WcqjActivity.java
@@ -0,0 +1,14 @@
+package com.sys8.sqjzapp.subModule.wcqj;
+
+import android.os.Bundle;
+import com.sys8.sqjzapp.R;
+import com.sys8.sqjzapp.baseClass.BaseActivity;
+
+public class WcqjActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wcqj);
+    }
+}
