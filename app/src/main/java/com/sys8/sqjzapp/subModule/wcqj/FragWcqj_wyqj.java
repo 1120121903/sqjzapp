8037516696d4249ce1e2c2db78958cab3798415b
1120121903 <460980971@qq.com>
2020-04-02 10:51:48 +0800

    外出请假-我要请假

--- app/src/main/java/com/sys8/sqjzapp/subModule/wcqj/FragWcqj_wyqj.java
+++ app/src/main/java/com/sys8/sqjzapp/subModule/wcqj/FragWcqj_wyqj.java
@@ -7,20 +7,24 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 
 import com.bigkoo.pickerview.builder.TimePickerBuilder;
 import com.bigkoo.pickerview.listener.OnTimeSelectListener;
 import com.bigkoo.pickerview.view.TimePickerView;
+import com.kingja.supershapeview.view.SuperShapeEditText;
 import com.sys8.sqjzapp.R;
 
+import java.util.Calendar;
 import java.util.Date;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
+import static com.sys8.sqjzapp.utils.DateUtils.calculateDaysFromTwoDateString;
 import static com.sys8.sqjzapp.utils.DateUtils.dateToString;
 
 public class FragWcqj_wyqj extends Fragment {
@@ -30,7 +34,19 @@ public class FragWcqj_wyqj extends Fragment {
     EditText etWcqjWyqjKssj;
     @BindView(R.id.et_wcqj_wyqj_jssj)
     EditText etWcqjWyqjJssj;
+    @BindView(R.id.et_wcqj_wyqj_wcsc)
+    EditText etWcqjWyqjWcsc;
+    @BindView(R.id.et_wcqj_wyqj_wcdd)
+    EditText etWcqjWyqjWcdd;
+    @BindView(R.id.et_wcqj_wyqj_wcly)
+    SuperShapeEditText etWcqjWyqjWcly;
+    @BindView(R.id.bt_wcqj_wyqj_save)
+    Button btWcqjWyqjSave;
     private WcqjActivity parentActivity = null;
+    private Calendar kssjCalendarStart;
+    private Calendar kssjCalendarEnd;
+    private Calendar jssjCalendarStart;
+    private Calendar jssjCalendarEnd;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -42,8 +58,8 @@ public class FragWcqj_wyqj extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.frag_wcqj_wyqj, container, false);
         // Inflate the layout for this fragment
-        parentActivity = (WcqjActivity) getActivity();
         ButterKnife.bind(this, view);
+        parentActivity = (WcqjActivity) getActivity();
         etWcqjWyqjKssj.setInputType(InputType.TYPE_NULL);
         etWcqjWyqjJssj.setInputType(InputType.TYPE_NULL);
         return view;
@@ -51,24 +67,46 @@ public class FragWcqj_wyqj extends Fragment {
 
     @OnClick(R.id.et_wcqj_wyqj_kssj)
     public void getKssj() {
+        kssjCalendarStart = Calendar.getInstance();
+        kssjCalendarEnd = Calendar.getInstance();
+        kssjCalendarEnd.add(kssjCalendarStart.YEAR, +1); //年份加1
         TimePickerView pvTime = new TimePickerBuilder(getContext(), new OnTimeSelectListener() {
             @Override
             public void onTimeSelect(Date date, View v) {
                 etWcqjWyqjKssj.setText(dateToString(date, "yyyy年MM月dd日"));
+                jssjCalendarStart = Calendar.getInstance();
+                jssjCalendarStart.setTime(date);
+                etWcqjWyqjJssj.setText("");
             }
-        }).build();
+        })
+                .setRangDate(kssjCalendarStart, kssjCalendarEnd)//设置起始时间和结束时间
+                .build();
         pvTime.show();
     }
 
     @OnClick(R.id.et_wcqj_wyqj_jssj)
     public void getJssj() {
+        if (etWcqjWyqjKssj.getText().toString().equals("")) {
+            Toast.makeText(getActivity(), "请选择开始时间", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        jssjCalendarEnd = Calendar.getInstance();
+        jssjCalendarEnd.add(jssjCalendarStart.YEAR, +1); //年份加1
         TimePickerView pvTime = new TimePickerBuilder(getContext(), new OnTimeSelectListener() {
             @Override
             public void onTimeSelect(Date date, View v) {
                 etWcqjWyqjJssj.setText(dateToString(date, "yyyy年MM月dd日"));
+                etWcqjWyqjWcsc.setText(calculateDaysFromTwoDateString(etWcqjWyqjKssj.getText().toString(),etWcqjWyqjJssj.getText().toString()));
             }
-        }).build();
+        })
+                .setRangDate(jssjCalendarStart, jssjCalendarEnd)//设置起始时间和结束时间
+                .build();
         pvTime.show();
+    }
+
+    @OnClick(R.id.bt_wcqj_wyqj_save)
+    public void saveData(){
+        parentActivity.wcqjQjjlFragment();
     }
 
     private FragWcqj_wyqj() {
--- app/src/main/java/com/sys8/sqjzapp/utils/DateUtils.java
+++ app/src/main/java/com/sys8/sqjzapp/utils/DateUtils.java
@@ -1,12 +1,47 @@
 package com.sys8.sqjzapp.utils;
 
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 public class DateUtils {
+
     // formatType格式为yyyy-MM-dd HH:mm:ss//yyyy年MM月dd日 HH时mm分ss秒
-    // data Date类型的时间
     public static String dateToString(Date data, String formatType) {
         return new SimpleDateFormat(formatType).format(data);
+    }
+
+    // formatType格式为yyyy-MM-dd HH:mm:ss//yyyy年MM月dd日 HH时mm分ss秒
+    public static Date stringToDate(String data, String formatType) {
+        try {
+            java.text.SimpleDateFormat formatter = new SimpleDateFormat(formatType);
+            Date date = formatter.parse(data);
+            return date;
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+        return null;
+    }
+
+    public static String calculateDaysFromTwoDateString(String string1,String string2){
+        Date date1 = stringToDate(string1,"yyyy年MM月dd日");
+        Date date2 = stringToDate(string2,"yyyy年MM月dd日");
+
+        Calendar fromCalendar = Calendar.getInstance();
+        fromCalendar.setTime(date1);
+        fromCalendar.set(Calendar.HOUR_OF_DAY, 0);
+        fromCalendar.set(Calendar.MINUTE, 0);
+        fromCalendar.set(Calendar.SECOND, 0);
+        fromCalendar.set(Calendar.MILLISECOND, 0);
+
+        Calendar toCalendar = Calendar.getInstance();
+        toCalendar.setTime(date2);
+        toCalendar.set(Calendar.HOUR_OF_DAY, 0);
+        toCalendar.set(Calendar.MINUTE, 0);
+        toCalendar.set(Calendar.SECOND, 0);
+        toCalendar.set(Calendar.MILLISECOND, 0);
+
+        return ((int) ((toCalendar.getTime().getTime() - fromCalendar.getTime().getTime()) / (1000 * 60 * 60 * 24)))+"天";
+
     }
 }
