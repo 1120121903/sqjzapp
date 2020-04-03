8248a75f5d8ec61ec0b8b9351c8fe80dc8a38b0d
guoxw <xuewei_g@163.com>
2020-04-03 11:53:37 +0800

    教育学习画个底部栏

--- app/src/main/java/com/sys8/sqjzapp/subModule/educationLearning/EducationLearningActivity.java
+++ app/src/main/java/com/sys8/sqjzapp/subModule/educationLearning/EducationLearningActivity.java
@@ -1,27 +1,37 @@
 package com.sys8.sqjzapp.subModule.educationLearning;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.Button;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.hjq.bar.TitleBar;
 import com.sys8.sqjzapp.R;
+import com.sys8.sqjzapp.adapters.MainFragmentAdapter;
 import com.sys8.sqjzapp.baseClass.ActivityCollector;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class EducationLearningActivity extends AppCompatActivity {
 
     @BindView(R.id.tb_educationlearning_titlebar)
     TitleBar tbEducationlearningTitlebar;
-    @BindView(R.id.bt_educationlearning_video_tab)
-    Button btEducationlearningVideoTab;
-    @BindView(R.id.bt_educationlearning_audio_tab)
-    Button btEducationlearningAudioTab;
-    @BindView(R.id.bt_educationlearning_picture_tab)
-    Button btEducationlearningPictureTab;
+    @BindView(R.id.vp_educationlearning_pager)
+    ViewPager vpEducationlearningPager;
+    @BindView(R.id.ecuation_bottom_nav_view)
+    BottomNavigationView ecuationBottomNavView;
+
+    private List<Fragment> mPageFragmentList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,5 +39,57 @@ public class EducationLearningActivity extends AppCompatActivity {
         setContentView(R.layout.activity_education_learning);
         ButterKnife.bind(this);
         ActivityCollector.TitleCilckListener(tbEducationlearningTitlebar, this);
+        initFragmentPageData(); /* 底部标签页绑定布局 */
+    }
+
+    /**
+     * description:底部标签页绑定布局，实现滑动切换效果
+     */
+    public void initFragmentPageData() {
+        mPageFragmentList = new ArrayList<Fragment>(3);
+        mPageFragmentList.add(new VideoEducationFragment());
+        mPageFragmentList.add(new AudioEducationFragment());
+        mPageFragmentList.add(new PictureEducationFragment());
+        //viewpager初始化
+        MainFragmentAdapter mainFragmentAdapter = new MainFragmentAdapter(getSupportFragmentManager(), mPageFragmentList);
+        vpEducationlearningPager.setAdapter(mainFragmentAdapter);
+
+        //viewpager滑动点击等监听
+        ecuationBottomNavView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int menuId = item.getItemId();
+                switch (menuId) {
+                    case R.id.bt_educationlearning_video_tab:
+                        vpEducationlearningPager.setCurrentItem(0);
+                        break;
+                    case R.id.bt_educationlearning_audio_tab:
+                        vpEducationlearningPager.setCurrentItem(1);
+                        break;
+                    case R.id.bt_educationlearning_picture_tab:
+                        vpEducationlearningPager.setCurrentItem(2);
+                        break;
+                }
+                return false;
+            }
+        });
+
+        vpEducationlearningPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                ecuationBottomNavView.getMenu().getItem(position).setChecked(true);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/sys8/sqjzapp/subModule/educationLearning/PictureEducationFragment.java
@@ -0,0 +1,29 @@
+package com.sys8.sqjzapp.subModule.educationLearning;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.sys8.sqjzapp.R;
+
+public class PictureEducationFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_picture_education, container, false);
+    }
+
+}
