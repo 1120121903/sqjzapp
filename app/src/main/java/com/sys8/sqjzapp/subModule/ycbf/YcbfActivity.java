3ac5d28b05d2b81cf4b7fa8103d550342c25f826
1120121903 <460980971@qq.com>
2020-04-07 13:31:49 +0800

    首页更改

--- app/src/main/java/com/sys8/sqjzapp/subModule/ycbf/YcbfActivity.java
+++ app/src/main/java/com/sys8/sqjzapp/subModule/ycbf/YcbfActivity.java
@@ -2,14 +2,25 @@ package com.sys8.sqjzapp.subModule.ycbf;
 
 import android.os.Bundle;
 
+import com.hjq.bar.TitleBar;
 import com.sys8.sqjzapp.R;
 import com.sys8.sqjzapp.baseClass.BaseActivity;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+import static com.sys8.sqjzapp.baseClass.ActivityCollector.TitleCilckListener;
+
 public class YcbfActivity extends BaseActivity {
+
+    @BindView(R.id.tb_ycbf)
+    TitleBar tbYcbf;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ycbf);
+        ButterKnife.bind(this);
+        TitleCilckListener(tbYcbf, this);/*title按钮监听*/
     }
 }
