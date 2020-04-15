37a9599490cb25b7bf29abcc7967229f4d87fbcd
guoxw <xuewei_g@163.com>
2020-04-15 11:45:55 +0800

    矫正小组

--- app/src/main/java/com/sys8/sqjzapp/subModule/jzxz/JzxzActivity.java
+++ app/src/main/java/com/sys8/sqjzapp/subModule/jzxz/JzxzActivity.java
@@ -2,14 +2,25 @@ package com.sys8.sqjzapp.subModule.jzxz;
 
 import android.os.Bundle;
 
+import com.hjq.bar.TitleBar;
 import com.sys8.sqjzapp.R;
+import com.sys8.sqjzapp.baseClass.ActivityCollector;
 import com.sys8.sqjzapp.baseClass.BaseActivity;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class JzxzActivity extends BaseActivity {
+
+    @BindView(R.id.tb_jzxz_titlebar)
+    TitleBar tbJzxzTitlebar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_jzxz);
+        ButterKnife.bind(this);
+        ActivityCollector.TitleCilckListener(tbJzxzTitlebar, this);
+
     }
 }
