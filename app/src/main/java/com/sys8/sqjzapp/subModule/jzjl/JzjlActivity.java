2767f524d6ab84bf775bd9ca80e3f1bdcd0a7af0
1120121903 <460980971@qq.com>
2020-04-15 10:26:46 +0800

    调整文件位置

--- app/src/main/java/com/sys8/sqjzapp/subModule/jzjl/JzjlActivity.java
+++ app/src/main/java/com/sys8/sqjzapp/subModule/jzjl/JzjlActivity.java
@@ -1,15 +1,29 @@
 package com.sys8.sqjzapp.subModule.jzjl;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
+import com.hjq.bar.TitleBar;
 import com.sys8.sqjzapp.R;
 import com.sys8.sqjzapp.baseClass.BaseActivity;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+import static com.sys8.sqjzapp.baseClass.ActivityCollector.TitleCilckListener;
+
 public class JzjlActivity extends BaseActivity {
+
+    @BindView(R.id.tb_jzjl)
+    TitleBar tbJzjl;
+    @BindView(R.id.lv_jzjl)
+    ListView lvJzjl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_jzjl);
+        ButterKnife.bind(this);
+        TitleCilckListener(tbJzjl, this);/*title按钮监听*/
     }
 }
